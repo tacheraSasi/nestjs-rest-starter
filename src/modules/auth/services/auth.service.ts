63d1239498f616f64e6512ef20b5c326dcaabcc5
tacheraSasi <tacherasasi@gmail.com>
2026-03-23 14:40:10 +0300

    correct return type

--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -24,7 +24,7 @@ export class AuthService {
     if (!user || !(await user.comparePassword(pass))) {
       throw new UnauthorizedException('Invalid credentials');
     }
-    return ExcludeFromObject(user,["password"]);
+    return user;
   }
 
   async forgotPassword(dto: ForgotPasswordDto) {
